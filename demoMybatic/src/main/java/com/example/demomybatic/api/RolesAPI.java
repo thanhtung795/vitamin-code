a965e17d1ef78cadb8ddd284ce9f6929b7d50333
thanhtung795 <vothanhtung795@gmail.com>
2024-08-06 07:01:10 +0700

    update cac API

--- demoMybatic/src/main/java/com/example/demomybatic/api/RolesAPI.java
+++ demoMybatic/src/main/java/com/example/demomybatic/api/RolesAPI.java
@@ -12,65 +12,70 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class RolesAPI {
     @Autowired
     private RoleService roleService;
+
     @GetMapping("/getAllRole")
     public ResponseEntity<?> getAllRole() {
-        Map<String,Object> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         try {
-            map.put("success",true);
-            map.put("data",roleService.getRoles());
-            map.put("message","successfully");
-        }catch (Exception e){
-            map.put("success",false);
-            map.put("data",null);
-            map.put("message","faill");
+            map.put("success", true);
+            map.put("data", roleService.getRoles());
+            map.put("message", "successfully");
+        } catch (Exception e) {
+            map.put("success", false);
+            map.put("data", null);
+            map.put("message", "faill");
             e.printStackTrace();
         }
         return ResponseEntity.ok(map);
     }
+
     @PostMapping("/saveRole")
     public ResponseEntity<?> saveRole(@RequestBody Roles role) {
-        Map<String,Object> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         try {
-            map.put("success",true);
-            map.put("data",roleService.saveRoles(role));
-            map.put("message","successfully");
-        }catch (Exception e){
-            map.put("success",false);
-            map.put("data",null);
-            map.put("message","faill");
+            map.put("success", true);
+            map.put("data", roleService.saveRoles(role));
+            map.put("message", "successfully");
+        } catch (Exception e) {
+            map.put("success", false);
+            map.put("data", null);
+            map.put("message", "faill");
             e.printStackTrace();
         }
         return ResponseEntity.ok(map);
     }
+
     @PutMapping("/updateRole")
     public ResponseEntity<?> updateRole(@RequestBody Roles role) {
-        Map<String,Object> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         try {
-            map.put("success",true);
-            map.put("data",roleService.updateRoles(role));
-            map.put("message","successfully");
-        }catch (Exception e){
-            map.put("success",false);
-            map.put("data",null);
-            map.put("message","faill");
+            map.put("success", true);
+            map.put("data", roleService.updateRoles(role));
+            map.put("message", "successfully");
+        } catch (Exception e) {
+            map.put("success", false);
+            map.put("data", null);
+            map.put("message", "faill");
             e.printStackTrace();
         }
         return ResponseEntity.ok(map);
     }
+
     @DeleteMapping("/deleteRole")
     public ResponseEntity<?> deleteRole(@RequestParam int id) {
-        Map<String,Object> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         try {
-            map.put("success",true);
-            map.put("data",roleService.deleteRoles(id));
-            map.put("message","successfully");
-        }catch (Exception e){
-            map.put("success",false);
-            map.put("data",null);
-            map.put("message","faill");
+            map.put("success", true);
+            map.put("data", roleService.deleteRoles(id));
+            map.put("message", "successfully");
+        } catch (Exception e) {
+            map.put("success", false);
+            map.put("data", null);
+            map.put("message", "faill");
             e.printStackTrace();
         }
         return ResponseEntity.ok(map);
